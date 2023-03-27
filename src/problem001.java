001147ce6dd059ddc5e0aa036033639cf278e556
PedroDCP05 <pedro.pereira0510@hotmail.com>
2023-03-26 23:33:44 -0300

    euler001

--- src/problem001.java
+++ src/problem001.java
@@ -1,7 +1,16 @@
 public class problem001 {
 
     public static void main(String[] args){
-        System.out.println("oi");
-        
+        System.out.println("Hello World! Project Euler 001.");
+        int x = 3;
+        int y = 5;
+        int sum = 0;
+
+        for(int i = 0; i < 1000;i++){
+            if(i % 3 == 0 || i % 5 == 0){
+                sum += i;
+            }
+        }
+        System.out.println("The sum of the multiples of " + x + " and " + y + " is: " + sum);
     }
 }
